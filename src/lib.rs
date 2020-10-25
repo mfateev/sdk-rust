191dcc887288448496b048400506638c86a4b109
Maxim Fateev <mfateev@gmail.com>
2020-10-24 17:52:22 -0700

    Added describe_namespace unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use temporal::api::workflowservice::v1::DescribeNamespaceRequest;
     use temporal::api::workflowservice::v1::workflow_service_client::WorkflowServiceClient;
 
     pub mod temporal {
@@ -85,8 +86,15 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn it_works() {
-        let mut client = WorkflowServiceClient::connect("http://[::1]:50051").await.unwrap();
-        assert_eq!(2 + 2, 4);
+    async fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+        let mut client = WorkflowServiceClient::connect("http://[::1]:7233").await?;
+        let request = tonic::Request::new(DescribeNamespaceRequest {
+            name: "default".into(),
+            id: "".into(),
+        });
+        let response = client.describe_namespace(request).await?;
+        println!("RESPONSE={:?}", response);
+        assert_eq!("default", response.get_ref().namespace_info.as_ref().unwrap().name);
+        Ok(())
     }
 }
